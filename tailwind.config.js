a901b613fd98b12bdab2ff5762cbc3c0141944b0
unknown <bensonagala@live.com>
2023-08-08 12:43:28 +0300

    project  setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,14 @@ module.exports = {
     "./plugins/**/*.{js,ts}",
     "./nuxt.config.{js,ts}",
     "./app.vue",
+    "./node_modules/flowbite/**/*.{js,ts}"
+
   ],
     theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
