394976c86f499ff383f9b3510476f3cb60d11117
unknown <bensonagala@live.com>
2023-08-09 17:39:01 +0300

    Home page setup

--- components/Footer.vue
+++ components/Footer.vue
@@ -296,12 +296,12 @@
                         Live Chat
                       </span>
       
-                      <span class="relative flex h-2 w-2">
+                      <span class="relative flex h-3 w-3">
                         <span
                           class="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 opacity-75"
                         ></span>
                         <span
-                          class="relative inline-flex h-2 w-2 rounded-full bg-red-500"
+                          class="relative inline-flex h-3 w-3 rounded-full bg-red-500"
                         ></span>
                       </span>
                     </a>
