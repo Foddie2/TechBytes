4c8cf05d117f566d162ea07268c5bc2d97ed8e5e
Foddie2 <Bensonagala@live.com>
2023-08-15 00:05:49 +0300

    Hero page cleanup

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -92,7 +92,7 @@
                     <button aria-label="Close Menu" title="Close Menu"
                       class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
                       @click="isMenuOpen = false">
-                      <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                      <svg class="w-5 text-gray-900" viewBox="0 0 24 24">
                         <path fill="currentColor"
                           d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z">
                         </path>
