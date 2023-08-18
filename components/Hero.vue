a10da5e350d2e585a77cba66fd9f68ec3d8ab99b
unknown <bensonagala@live.com>
2023-08-18 11:33:11 +0300

    Home page setup

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,39 +1,57 @@
 <template>
- <!-- component -->
-<section>
-	<div class="bg-black text-white py-8 ">
-		<div class="container mx-auto flex flex-col md:flex-row items-center my-8 md:my-24 px-4 pt-16 sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-12 lg:px-4">
-			<div class="flex flex-col w-full lg:w-1/3 justify-center items-start p-4">
-				<h1 class="text-3xl md:text-6xl p-4 text-green-300 tracking-loose">TechBytes Today</h1>
-				<h2 class="text-3xl md:text-6xl leading-relaxed md:leading-snug mb-2">Endless IT Possibilities, Delivered.
-				</h2>
-				<p class="text-sm md:text-base text-gray-50 mb-4">Where imagination meets innovation, We do everything with you in mind! We help you grow your business.
-				</p>
-				<a href="#"
-					class="bg-transparent hover:bg-green-300 text-green-300 hover:text-black rounded shadow hover:shadow-lg py-2 px-4 border border-green-300 hover:border-transparent">
-					Explore Now</a>
-			</div>
-			<div class="p-8 mt-12 mb-6 md:mb-0 md:mt-0 ml-0 md:ml-12 lg:w-2/3  justify-center">
-				<div class="h-48 flex flex-wrap content-center">
-					<div>
-						<img class="inline-block mt-28 hidden xl:block" src="https://user-images.githubusercontent.com/54521023/116969935-c13d5b00-acd4-11eb-82b1-5ad2ff10fb76.png"></div>
-						<div>
-							<img class="inline-block mt-24 md:mt-0 p-8 md:p-0"  src="https://user-images.githubusercontent.com/54521023/116969931-bedb0100-acd4-11eb-99a9-ff5e0ee9f31f.png"></div>
-							<div>
-								<img class="inline-block mt-28 hidden lg:block" src="https://user-images.githubusercontent.com/54521023/116969939-c1d5f180-acd4-11eb-8ad4-9ab9143bdb50.png"></div>
-							</div>
-						</div>
-					</div>
-				</div>
-</section>
+  <!-- component -->
+  <section>
+    <div class="bg-black text-white py-8">
+      <div
+        class="container mx-auto flex flex-col md:flex-row items-center my-8 md:my-24 px-4 pt-16 sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-12 lg:px-4"
+      >
+        <div class="flex flex-col w-full lg:w-1/3 justify-center items-start p-4">
+          <h1 class="text-3xl md:text-6xl p-4 text-green-300 tracking-loose">
+            TechBytes Today
+          </h1>
+          <h2 class="text-3xl md:text-6xl leading-relaxed md:leading-snug mb-2">
+            Endless IT Possibilities, Delivered.
+          </h2>
+          <p class="text-sm md:text-base text-gray-50 mb-4">
+            Where imagination meets innovation, We do everything with you in mind! We help
+            you grow your business.
+          </p>
+          <a
+            href="#"
+            class="bg-transparent hover:bg-green-300 text-green-300 hover:text-black rounded shadow hover:shadow-lg py-2 px-4 border border-green-300 hover:border-transparent"
+          >
+            Explore Now</a
+          >
+        </div>
+        <div class="p-8 mt-12 mb-6 md:mb-0 md:mt-0 ml-0 md:ml-12 lg:w-2/3 justify-center">
+          <div class="h-48 flex flex-wrap content-center">
+            <div>
+              <img
+                class="inline-block mt-28 hidden xl:block"
+                src="https://user-images.githubusercontent.com/54521023/116969935-c13d5b00-acd4-11eb-82b1-5ad2ff10fb76.png"
+              />
+            </div>
+            <div>
+              <img
+                class="inline-block mt-24 md:mt-0 p-8 md:p-0"
+                src="https://user-images.githubusercontent.com/54521023/116969931-bedb0100-acd4-11eb-99a9-ff5e0ee9f31f.png"
+              />
+            </div>
+            <div>
+              <img
+                class="inline-block mt-28 hidden lg:block"
+                src="https://user-images.githubusercontent.com/54521023/116969939-c1d5f180-acd4-11eb-8ad4-9ab9143bdb50.png"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
