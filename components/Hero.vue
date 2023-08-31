bd123615f3e9222a36c723068ab448086a692a1f
Foddie2 <Bensonagala@live.com>
2023-08-31 23:56:33 +0300

    cleanup

--- components/Hero.vue
+++ components/Hero.vue
@@ -26,19 +26,19 @@
         <div class="p-8 mt-12 mb-6 md:mb-0 md:mt-0 ml-0 md:ml-12 lg:w-2/3 justify-center">
           <div class="h-48 flex flex-wrap content-center">
             <div>
-              <img
+              <NuxtImg
                 class="inline-block mt-28 hidden xl:block"
                 src="https://user-images.githubusercontent.com/54521023/116969935-c13d5b00-acd4-11eb-82b1-5ad2ff10fb76.png"
               />
             </div>
             <div>
-              <img
+              <NuxtImg
                 class="inline-block mt-24 md:mt-0 p-8 md:p-0"
                 src="https://user-images.githubusercontent.com/54521023/116969931-bedb0100-acd4-11eb-99a9-ff5e0ee9f31f.png"
               />
             </div>
             <div>
-              <img
+              <NuxtImg
                 class="inline-block mt-28 hidden lg:block"
                 src="https://user-images.githubusercontent.com/54521023/116969939-c1d5f180-acd4-11eb-8ad4-9ab9143bdb50.png"
               />
