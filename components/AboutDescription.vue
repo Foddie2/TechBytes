53cc3c882354a9639db96cbeb9581e70b7a48416
unknown <bensonagala@live.com>
2023-09-11 12:25:39 +0300

    our story

--- components/AboutDescription.vue
+++ components/AboutDescription.vue
@@ -1,32 +1,33 @@
 <template>
-<!-- component -->
-<div class=" max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8 mx-auto">
+    <div class="max-w-screen-xl sm:px-6 sm:py-6 lg:px-8 mx-auto">
+
     <main class="mt-10">
       <div class="block md:flex md:space-x-2 px-2 lg:p-0">
         <a 
           class="mb-4 md:mb-0 w-full md:w-2/3 relative rounded inline-block" 
           style="height: 24em;"
-          href=""
+          href="#"
         >
           <div class="absolute left-0 bottom-0 w-full h-full z-10"
             style="background-image: linear-gradient(180deg,transparent,rgba(0,0,0,.7));"></div>
-          <img src="https://images.pexels.com/photos/1291712/pexels-photo-1291712.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="absolute left-0 top-0 w-full h-full rounded z-0 object-cover" />
-          <div class="p-4 absolute bottom-0 left-0">
-            <span class="px-4 py-1 bg-pink-900 text-white inline-flex items-center justify-center mb-2">Nutrition</span>
-            <h2 class="text-4xl font-semibold text-white leading-tight">
+          <img src="https://images.unsplash.com/photo-1493770348161-369560ae357d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80" class="absolute left-0 top-0 w-full h-full rounded z-0 object-cover" />
+          <div class="p-4 absolute bottom-0 left-0 z-10">
+            <span class="px-4 py-1 bg-black text-gray-200 inline-flex items-center justify-center mb-2">Nutrition</span>
+            <h2 class="text-4xl font-semibold text-gray-100 leading-tight">
               Pellentesque a consectetur velit, ac molestie ipsum. Donec sodales, massa et auctor.
             </h2>
-           <!--  <div class="flex mt-3">
+            <div class="flex mt-3">
               <img src="https://randomuser.me/api/portraits/men/97.jpg"
                 class="h-10 w-10 rounded-full mr-2 object-cover" />
               <div>
                 <p class="font-semibold text-gray-200 text-sm"> Mike Sullivan </p>
                 <p class="font-semibold text-gray-400 text-xs"> 14 Aug </p>
               </div>
-            </div> -->
+            </div>
           </div>
         </a>
 
+       
         <div class="w-full md:w-1/3 relative rounded" 
           style="height: 24em;"
           href=""
@@ -49,21 +50,17 @@
             </div> -->
           </div>
         </div>
-    </div>
-
-
-          
-
+</div>
       <div class="block lg:flex lg:space-x-2 px-2 lg:p-0 mt-10 mb-10">
         <!-- post cards -->
         <div class="w-full lg:w-2/3">
 
           <a class="block rounded w-full lg:flex mb-10"
-            href=""
+            href="#"
           >
             <div 
               class="h-48 lg:w-48 flex-none bg-cover text-center overflow-hidden opacity-75"
-              style="background-image: url('https://images.pexels.com/photos/913134/pexels-photo-913134.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')" 
+              style="background-image: url('https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80')" 
               title="deit is very important"
             >
             </div>
@@ -77,20 +74,20 @@
                   Pellentesque sed neque vel tellus lacinia elementum. Proin consequat ullamcorper eleifend.
                 </p>
               </div>
-             <!--  <div class="flex mt-3">
+              <div class="flex mt-3">
                 <img src="https://randomuser.me/api/portraits/men/86.jpg"
                   class="h-10 w-10 rounded-full mr-2 object-cover" />
                 <div>
                   <p class="font-semibold text-gray-700 text-sm capitalize"> eduard franz </p>
                   <p class="text-gray-600 text-xs"> 14 Aug </p>
                 </div>
-              </div> -->
+              </div>
             </div>
           </a>
 
           <div class="rounded w-full lg:flex mb-10">
             <div class="h-48 lg:w-48 flex-none bg-cover text-center overflow-hidden opacity-75"
-              style="background-image: url('https://images.pexels.com/photos/853005/pexels-photo-853005.jpeg?auto=compress&cs=tinysrgb&w=600')" title="deit is very important">
+              style="background-image: url('https://images.unsplash.com/photo-1515003197210-e0cd71810b5f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80')" title="deit is very important">
             </div>
             <div class="bg-white rounded px-4 flex flex-col justify-between leading-normal">
               <div>
@@ -104,20 +101,20 @@
                   Nam bibendum risus sit amet metus semper consectetur.
                 </p>
               </div>
-            <!--   <div class="flex mt-3">
+              <div class="flex mt-3">
                 <img src="https://randomuser.me/api/portraits/women/54.jpg"
                   class="h-10 w-10 rounded-full mr-2 object-cover" />
                 <div>
                   <p class="font-semibold text-gray-700 text-sm capitalize"> Serenity Hughes </p>
                   <p class="text-gray-600 text-xs"> 14 Aug </p>
                 </div>
-              </div> -->
+              </div>
             </div>
           </div>
 
           <div class="rounded w-full lg:flex mb-10">
             <div class="h-48 lg:w-48 flex-none bg-cover text-center overflow-hidden opacity-75"
-              style="background-image: url('https://images.pexels.com/photos/10281284/pexels-photo-10281284.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')" title="deit is very important">
+              style="background-image: url('https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80')" title="deit is very important">
             </div>
             <div class="bg-white rounded px-4 flex flex-col justify-between leading-normal">
               <div>
@@ -129,14 +126,14 @@
                   vitae eleifend odio ipsum at odio. Phasellus luctus scelerisque felis eget suscipit. 
                 </p>
               </div>
-            <!--   <div class="flex mt-3">
+              <div class="flex mt-3">
                 <img src="https://randomuser.me/api/portraits/men/86.jpg"
                   class="h-10 w-10 rounded-full mr-2 object-cover" />
                 <div>
                   <p class="font-semibold text-gray-700 text-sm capitalize"> eduard franz </p>
                   <p class="text-gray-600 text-xs"> 14 Aug </p>
                 </div>
-              </div> -->
+              </div>
             </div>
           </div>
 
@@ -146,37 +143,37 @@
         <div class="w-full lg:w-1/3 px-3">
           <!-- topics -->
           <div class="mb-4">
-            <h5 class="font-bold text-lg uppercase text-gray-700 px-1 mb-2"> Popular Flavours </h5>
+            <h5 class="font-bold text-lg uppercase text-gray-700 px-1 mb-2"> Popular Topics </h5>
             <ul>
               <li class="px-1 py-4 border-b border-t border-white hover:border-gray-200 transition duration-300">
-                <a href="#" class="flex items-center text-gray-700 cursor-pointer">
-                  <span class="inline-block h-4 w-4 bg-pink-300 mr-3"></span>
-                  Bubblegum Cakes
-                  <span class="text-gray-500 ml-auto">+ 150 Orders</span>
-                  <i class='text-gray-500 bx bx-right-arrow-alt ml-1'></i>
-                </a>
-              </li>
-              <li class="px-1 py-4 border-b border-t border-white hover:border-gray-200 transition duration-300">
-                <a href="#" class="flex items-center text-gray-700 cursor-pointer">
-                  <span class="inline-block h-4 w-4 bg-pink-500 mr-3"></span>
-                  Strawberry Cakes
-                  <span class="text-gray-500 ml-auto">+ 250 Orders</span>
+                <a href="#" class="flex items-center text-gray-600 cursor-pointer">
+                  <span class="inline-block h-4 w-4 bg-green-300 mr-3"></span>
+                  Nutrition
+                  <span class="text-gray-500 ml-auto">23 articles</span>
                   <i class='text-gray-500 bx bx-right-arrow-alt ml-1'></i>
                 </a>
               </li>
               <li class="px-1 py-4 border-b border-t border-white hover:border-gray-200 transition duration-300">
                 <a href="#" class="flex items-center text-gray-600 cursor-pointer">
-                  <span class="inline-block h-4 w-4 bg-pink-700 mr-3"></span>
-                  Vanilla Cakes
-                  <span class="text-gray-500 ml-auto">+ 300 Orders</span>
+                  <span class="inline-block h-4 w-4 bg-indigo-300 mr-3"></span>
+                  Food & Diet
+                  <span class="text-gray-500 ml-auto">18 articles</span>
                   <i class='text-gray-500 bx bx-right-arrow-alt ml-1'></i>
                 </a>
               </li>
               <li class="px-1 py-4 border-b border-t border-white hover:border-gray-200 transition duration-300">
                 <a href="#" class="flex items-center text-gray-600 cursor-pointer">
-                  <span class="inline-block h-4 w-4 bg-pink-200 mr-3"></span>
-                  Blackforest Cakes
-                  <span class="text-gray-500 ml-auto">+ 100 Orders</span>
+                  <span class="inline-block h-4 w-4 bg-yellow-300 mr-3"></span>
+                  Workouts
+                  <span class="text-gray-500 ml-auto">34 articles</span>
+                  <i class='text-gray-500 bx bx-right-arrow-alt ml-1'></i>
+                </a>
+              </li>
+              <li class="px-1 py-4 border-b border-t border-white hover:border-gray-200 transition duration-300">
+                <a href="#" class="flex items-center text-gray-600 cursor-pointer">
+                  <span class="inline-block h-4 w-4 bg-blue-300 mr-3"></span>
+                  Immunity
+                  <span class="text-gray-500 ml-auto">9 articles</span>
                   <i class='text-gray-500 bx bx-right-arrow-alt ml-1'></i>
                 </a>
               </li>
@@ -193,8 +190,8 @@
               Subscribe to our newsletter. We deliver the best health related articles to your inbox
             </p>
             <input placeholder="your email address"
-              class="text-gray-700 bg-pink-50 rounded-t hover:outline-none p-2 w-full mt-4 border" />
-            <button class="px-4 py-2 bg-pink-700 text-white rounded-b w-full capitalize tracking-wide">
+              class="text-gray-700 bg-gray-100 rounded-t hover:outline-none p-2 w-full mt-4 border" />
+            <button class="px-4 py-2 bg-indigo-600 text-gray-200 rounded-b w-full capitalize tracking-wide">
               Subscribe
             </button>
           </div>
