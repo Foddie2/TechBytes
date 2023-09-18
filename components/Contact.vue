e98eeaa2eb464a9208656c86b5bf5adb585763bd
Foddie2 <Bensonagala@live.com>
2023-09-18 15:58:07 +0300

    cleanup

--- components/Contact.vue
+++ components/Contact.vue
@@ -16,7 +16,7 @@
                       d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
                       clip-rule="evenodd"></path>
                   </svg>
-                  <span>Riruta Area, Ndwaru Road</span>
+                  <span>Donholm Area, Nairobi</span>
                 </p>
                 <p class="flex items-center">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
@@ -25,7 +25,7 @@
                       d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
                       clip-rule="evenodd"></path>
                   </svg>
-                  <span>P.O BOX 21200-00100,</span>
+                  <span>P.O BOX 30126 - 00100,</span>
                 </p>
                 <p class="flex items-center">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
@@ -34,7 +34,7 @@
                       d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z">
                     </path>
                   </svg>
-                  <span> +254729830432 / +254740261022</span>
+                  <span> +254711974716</span>
                 </p>
               </div>
               <form>
