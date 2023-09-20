546672728b21eebd7fe439784afaad766f6bf83f
unknown <bensonagala@live.com>
2023-09-20 17:33:11 +0300

    Home cleanup

--- components/About.vue
+++ components/About.vue
@@ -19,7 +19,7 @@
         </div>
 
         <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-16 md:grid-cols-2 xl:grid-cols-3">
-            <div class="flex flex-col items-center p-6 space-y-3 text-center bg-green-50 rounded-xl dark:bg-gray-800 transition duration-300 transform  shadow-sm hover:scale-105 group hover:shadow-xl lg:hover:bg-white">
+            <div class="flex flex-col items-center p-6 space-y-3 text-center bg-green-50 rounded-xl dark:bg-gray-800 transition duration-300 transform  shadow-sm hover:scale-105 group hover:shadow-xl lg:hover:bg-white sm:hover:bg-white">
                 <span class="inline-block p-3 text-green-600 bg-green-100 rounded-full dark:text-white dark:bg-green-600">
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
@@ -38,7 +38,7 @@
                 </a>
             </div>
 
-            <div class="flex flex-col items-center p-6 space-y-3 text-center bg-green-50 rounded-xl dark:bg-gray-800 transition duration-300 transform  shadow-sm hover:scale-105 group hover:shadow-xl lg:hover:bg-white">
+            <div class="flex flex-col items-center p-6 space-y-3 text-center bg-green-50 rounded-xl dark:bg-gray-800 transition duration-300 transform  shadow-sm hover:scale-105 group hover:shadow-xl lg:hover:bg-white sm:hover:bg-white">
                 <span class="inline-block p-3 text-green-600 bg-green-100 rounded-full dark:text-white dark:bg-green-600">
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
@@ -57,7 +57,7 @@
                 </a>
             </div>
 
-            <div class="flex flex-col items-center p-6 space-y-3 text-center bg-green-50 rounded-xl dark:bg-gray-800 transition duration-300 transform  shadow-sm hover:scale-105 group hover:shadow-xl lg:hover:bg-white">
+            <div class="flex flex-col items-center p-6 space-y-3 text-center bg-green-50 rounded-xl dark:bg-gray-800 transition duration-300 transform  shadow-sm hover:scale-105 group hover:shadow-xl lg:hover:bg-white sm:hover:bg-white ">
                 <span class="inline-block p-3 text-green-600 bg-green-100 rounded-full dark:text-white dark:bg-green-600">
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
