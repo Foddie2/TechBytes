2946c84eec1c93649e0ec57fdd2ebd24fd46408f
unknown <bensonagala@live.com>
2023-09-21 16:47:27 +0300

    Home cleanup

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -197,7 +197,7 @@
                     </NuxtLink>
                   </li> -->
                   <li class="relative parent font-medium tracking-wide hover:text-green-500 uppercase ">services
-                    <a href="#" @click="isMenuOpen = true" class="flex justify-between md:inline-flex p-2 items-center space-x-2">
+                    <a href="#" class="flex justify-between md:inline-flex p-2 items-center space-x-2">
                           <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 fill-current pt-1" viewBox="0 0 24 24"><path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z"/></svg>
                         </a>
                           <ul class="child transition duration-300 md:absolute top-full right-0 md:w-52 bg-white md:shadow-lg md:rounded-b ">
