dc7295b4efb0b584e1cdaf0fa5dad81bd05565b3
Foddie2 <Bensonagala@live.com>
2023-09-24 22:03:00 +0300

    hero cleanup

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,22 +1,42 @@
 <template>
-  <!-- component -->
-  <section>
-    <div class="bg-stone-900 text-white py-4">
-      <div
-        class="container mx-auto flex flex-col md:flex-row items-center my-6 md:my-24 px-4  sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-12 lg:px-4"
-      >
-        <div class="flex flex-col w-full lg:w-1/3 justify-center items-start p-4">
-          <h1 class="text-3xl md:text-md p-4 text-green-400 tracking-loose  text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-teal-400">
-            Powering future
-          </h1>
-          <h2 class="text-3xl md:text-6xl leading-relaxed md:leading-snug mb-2">
-            Empowering Businesses through Technology          </h2>
-          <p class="text-sm md:text-base text-green-50 mb-4">
-            Where imagination meets innovation, We do everything with you in mind! We help
-            you grow your business.
-          </p>
-          
 
+
+<section
+  class="relative bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat"
+>
+  <div
+    class="absolute inset-0 bg-green/75 sm:bg-transparent sm:from-green/95 sm:to-white/25 ltr:sm:bg-gradient-to-r bg-gradient-to-r from-green-400 to-teal-400"
+  ></div>
+
+  <div
+    class="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8"
+  >
+    <div class="max-w-xl ltr:sm:text-left">
+      <h1 class="text-3xl font-extrabold sm:text-5xl text-gray-50">
+        Empowering Businesses through 
+        <strong class="block font-extrabold text-green-700">
+          Technology
+        </strong>
+      </h1>
+
+      <p class="mt-4 max-w-lg sm:text-xl/relaxed text-start">
+        Where imagination meets innovation, We do everything with you in mind! We help you grow your business.
+        
+      </p>
+
+      <div class="mt-8 flex flex-wrap gap-4 text-center">
+      
+        <a class="relative inline-flex items-center px-12 py-3 my-5 overflow-hidden text-lg font-medium text-green-400 border-2 border-green-400 rounded-full hover:text-black group hover:bg-green-400">
+          <span class="absolute left-0 block w-full h-0 transition-all bg-green-400 opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease group-hover:bg-gradient-to-r from-[#78f2b3] to-[#16f0de]"></span>
+          <span class="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
+          <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+          </span>
+          <NuxtLink to="/our-story">
+          <span class="relative uppercase">Explore now</span>
+        </NuxtLink>
+          </a>
+
+      
           <a class="relative inline-flex items-center px-12 py-3 my-5 overflow-hidden text-lg font-medium text-green-400 border-2 border-green-400 rounded-full hover:text-black group hover:bg-green-400">
             <span class="absolute left-0 block w-full h-0 transition-all bg-green-400 opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease group-hover:bg-gradient-to-r from-[#78f2b3] to-[#16f0de]"></span>
             <span class="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
@@ -26,37 +46,18 @@
             <span class="relative uppercase">Explore now</span>
           </NuxtLink>
             </a>
-         
-        </div>
-        <div class="p-8 mt-12 mb-6 md:mb-0 md:mt-0 ml-0 md:ml-12 lg:w-2/3 justify-center">
-          <div class="h-48 flex flex-wrap content-center">
-            <div>
-              <Img
-                class="inline-block mt-28 hidden xl:block"
-                src="https://user-images.githubusercontent.com/54521023/116969935-c13d5b00-acd4-11eb-82b1-5ad2ff10fb76.png"
-              />
-            </div>
-            <div>
-              <Img
-                class="inline-block mt-24 md:mt-0 p-8 md:p-0"
-                src="https://user-images.githubusercontent.com/54521023/116969931-bedb0100-acd4-11eb-99a9-ff5e0ee9f31f.png"
-              />
-            </div>
-            <div>
-              <Img
-                class="inline-block mt-28 hidden lg:block"
-                src="https://user-images.githubusercontent.com/54521023/116969939-c1d5f180-acd4-11eb-8ad4-9ab9143bdb50.png"
-              />
-            </div>
-          </div>
-        </div>
       </div>
     </div>
-  </section>
+  </div>
+</section>
 </template>
 
 <script>
-export default {};
+  export default {
+    
+  }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+</style>
