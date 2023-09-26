b2d45ea306556adfdecc8c70ef2719641afa98dd
unknown <bensonagala@live.com>
2023-09-26 16:01:16 +0300

    Content setup

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,5 +1,5 @@
 <template>
     <main>
-      <ContentDoc class="prose"/>
+      <ContentDoc class="prose px-4 py-6 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8" />
     </main>
   </template>
