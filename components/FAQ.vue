737f1d43c24fd5ec2c08d6b19b1a7aa7541e6ab8
unknown <bensonagala@live.com>
2023-09-29 17:35:55 +0300

    FAQ cleanup

--- components/FAQ.vue
+++ components/FAQ.vue
@@ -1,171 +1,160 @@
-<template>
-    <div class="max-w-screen-xl mx-auto px-5 bg-white min-h-sceen">
-	<div class="flex flex-col items-center">
-		<h2 class="font-bold text-5xl mt-5 tracking-tight">
-			FAQ
-		</h2>
-		<p class="text-neutral-500 text-xl mt-3">
-			Frequenty asked questions
-		</p>
-	</div>
-	<div class="grid divide-y divide-neutral-200 max-w-xl mx-auto mt-8">
-		<div class="py-5">
-			<details class="group">
-				<summary class="flex justify-between items-center font-medium cursor-pointer list-none">
-					<span> What is the cost of IT services?</span>
-					<span class="transition group-open:rotate-180">
-                <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
-</svg>
-              </span>
-				</summary>
-				<p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
-					The cost of IT services can vary widely depending on several factors, including the type of service, the scale of your business, your specific needs, at Techbytes, we provide cheap full support to scale up your business needs.
-				</p>
-			</details>
+	<template>
+		<div class="max-w-screen-xl mx-auto px-5 bg-white min-h-sceen">
+		<div class="flex flex-col items-center">
+			<h2 class="font-bold text-5xl mt-5 tracking-tight">
+				FAQ
+			</h2>
+			<p class="text-neutral-500 text-xl mt-3">
+				Frequenty asked questions
+			</p>
 		</div>
-		<div class="py-5">
-			<details class="group">
-				<summary class="flex justify-between items-center font-medium cursor-pointer list-none">
-					<span> How can IT services benefit small businesses?</span>
-					<span class="transition group-open:rotate-180">
-                <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
-</svg>
-              </span>
-				</summary>
-				<p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
-					IT services offer numerous benefits to small businesses, helping them become more efficient, competitive, and secure.  Here are some ways in which our IT services can benefit small businesses: Cost Savings, Increased Productivity, Improved Security, Business Continuity, Scalability, Access to Expertise, Compliance and Data Protection, 24/7 Support, Innovation and Competitive Edge, Focus on Core Competencies, Remote Work Support, Customer Service Improvement 	
-				</p>
-			</details>
-		</div>
-		<div class="py-5">
-			<details class="group">
-				<summary class="flex justify-between items-center font-medium cursor-pointer list-none">
-					<span> How can IT services benefit my business's overall productivity and efficiency?</span>
-					<span class="transition group-open:rotate-180">
-                <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
-</svg>
-              </span>
-				</summary>
-				<p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
-					IT services can significantly benefit your business's overall productivity and efficiency in the following ways
-
-Streamlined Operations, Enhanced Communication, Increased Data Accessibility, Efficient Data Management, Scalability, Automated Tasks, Customer Relationship Management (CRM), Data Analytics, IT Support and Issue Resolution, Cloud Computing, Security Measures, Disaster Recovery, Proactive Monitoring, Compliance and Reporting, Business Intelligence.
-				</p>
-			</details>
-		</div>
-		<div class="py-5">
-			<details class="group">
-				<summary class="flex justify-between items-center font-medium cursor-pointer list-none">
-					<span> What is managed IT services, and how does it work?</span>
-					<span class="transition group-open:rotate-180">
-                <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
-</svg>
-              </span>
-				</summary>
-				<p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
-					This involves outsourcing the management and maintenance of a business's IT infrastructure and systems to a third-party provider. The goal of managed IT services is to ensure the optimal functionality, security, and performance of a company's IT environment while allowing the business to focus on its core operations.
-				</p>
-			</details>
-		</div>
-		<div class="py-5">
-			<details class="group">
-				<summary class="flex justify-between items-center font-medium cursor-pointer list-none">
-					<span>Can you provide a roadmap for technology adoption and development for my business?</span>
-					<span class="transition group-open:rotate-180">
-                <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
-</svg>
-              </span>
-				</summary>
-				<p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
-					We offers a free trial of its  platform for a limited time. During the trial period,
-					you will have access to a limited set of features and functionality, but you will not be charged.
-				</p>
-			</details>
-		</div>
-		<div class="py-5">
-			<details class="group">
-				<summary class="flex justify-between items-center font-medium cursor-pointer list-none">
-					<span> Is 24/7 technical support included in your managed IT services package?</span>
-					<span class="transition group-open:rotate-180">
-                <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
-</svg>
-              </span>
-				</summary>
-				<p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
-					  Documentation is available on the company's website and can be accessed by
-					logging in to your account. The documentation provides detailed information on how to use the ,
-					as well as code examples and other resources.
-				</p>
-			</details>
-		</div>
-		<div class="py-5">
-			<details class="group">
-				<summary class="flex justify-between items-center font-medium cursor-pointer list-none">
-					<span> Do you offer IT services for small businesses as well as large enterprises?</span>
-					<span class="transition group-open:rotate-180">
-                <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
-</svg>
-              </span>
-				</summary>
-				<p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
-					If you need help with the platform or have any other questions, you can contact the
-					company's support team by submitting a support request through the website or by emailing
-					support@We.com.
-				</p>
-			</details>
-		</div>
-		<div class="py-5">
-			<details class="group">
-				<summary class="flex justify-between items-center font-medium cursor-pointer list-none">
-					<span> What makes your IT services different from other providers in the market?</span>
-					<span class="transition group-open:rotate-180">
-                <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
-</svg>
-              </span>
-				</summary>
-				<p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
-					We may offer discounts or promotions from time to time. To stay up-to-date on the latest
-					deals and special offers, you can sign up for the company's newsletter or follow it on social media.
-				</p>
-			</details>
-		</div>
-		<div class="py-5">
-			<details class="group">
-				<summary class="flex justify-between items-center font-medium cursor-pointer list-none">
-					<span> Can you customize a managed IT services plan for my specific business needs?</span>
-					<span class="transition group-open:rotate-180">
-                <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
-</svg>
-              </span>
-				</summary>
-				<p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
-					 This platform is a highly reliable and feature-rich service that offers a wide range
-					of tools and functionality. It is competitively priced and offers a variety of billing options to
-					suit different needs and budgets.
-				</p>
-			</details>
+		<div class="grid divide-y divide-neutral-200 max-w-xl mx-auto mt-8">
+			<div class="py-5">
+				<details class="group">
+					<summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+						<span> What is the cost of IT services?</span>
+						<span class="transition group-open:rotate-180">
+					<svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path></svg>
+				</span>
+					</summary>
+					<p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
+						The cost of IT services can vary widely depending on several factors, including the type of service, the scale of your business, your specific needs, at Techbytes, we provide cheap full support to scale up your business needs.
+					</p>
+				</details>
+			</div>
+			<div class="py-5">
+				<details class="group">
+					<summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+						<span> How can IT services benefit small businesses?</span>
+						<span class="transition group-open:rotate-180">
+					<svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path></svg>
+				</span>
+					</summary>
+					<p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
+						IT services offer numerous benefits to small businesses, helping them become more efficient, competitive, and secure.  Here are some ways in which our IT services can benefit small businesses: Cost Savings, Increased Productivity, Improved Security, Business Continuity, Scalability, Access to Expertise, Compliance and Data Protection, 24/7 Support, Innovation and Competitive Edge, Focus on Core Competencies, Remote Work Support, Customer Service Improvement 	
+					</p>
+				</details>
+			</div>
+			<div class="py-5">
+				<details class="group">
+					<summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+						<span> How can IT services benefit my business's overall productivity and efficiency?</span>
+						<span class="transition group-open:rotate-180">
+					<svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path></svg>
+				</span>
+					</summary>
+					<p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
+						IT services can significantly benefit your business's overall productivity and efficiency in the following ways: Streamlined Operations, Enhanced Communication, Increased Data Accessibility, Efficient Data Management, Scalability, Automated Tasks, Customer Relationship Management (CRM), Data Analytics, IT Support and Issue Resolution, Cloud Computing, Security Measures, Disaster Recovery, Proactive Monitoring, Compliance and Reporting, Business Intelligence.
+					</p>
+				</details>
+			</div>
+			<div class="py-5">
+				<details class="group">
+					<summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+						<span> What is managed IT services, and how does it work?</span>
+						<span class="transition group-open:rotate-180">
+					<svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path></svg>
+				</span>
+					</summary>
+					<p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
+						This involves outsourcing the management and maintenance of a business's IT infrastructure and systems to a third-party provider. The goal of managed IT services is to ensure the optimal functionality, security, and performance of a company's IT environment while allowing the business to focus on its core operations.
+					</p>
+				</details>
+			</div>
+			<div class="py-5">
+				<details class="group">
+					<summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+						<span>Can you provide a roadmap for technology adoption and development for my business?</span>
+						<span class="transition group-open:rotate-180">
+					<svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path></svg>
+				</span>
+					</summary>
+					<p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
+						We offers a free trial of its  platform for a limited time. During the trial period,
+						you will have access to a limited set of features and functionality, but you will not be charged.
+					</p>
+				</details>
+			</div>
+			<div class="py-5">
+				<details class="group">
+					<summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+						<span> Is 24/7 technical support included in your managed IT services package?</span>
+						<span class="transition group-open:rotate-180">
+					<svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path></svg>
+				</span>
+					</summary>
+					<p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
+						Documentation is available on the company's website and can be accessed by
+						logging in to your account. The documentation provides detailed information on how to use the ,
+						as well as code examples and other resources.
+					</p>
+				</details>
+			</div>
+			<div class="py-5">
+				<details class="group">
+					<summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+						<span> Do you offer IT services for small businesses as well as large enterprises?</span>
+						<span class="transition group-open:rotate-180">
+					<svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path></svg>
+				</span>
+					</summary>
+					<p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
+						If you need help with the platform or have any other questions, you can contact the
+						company's support team by submitting a support request through the website or by emailing
+						support@We.com.
+					</p>
+				</details>
+			</div>
+			<div class="py-5">
+				<details class="group">
+					<summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+						<span> What makes your IT services different from other providers in the market?</span>
+						<span class="transition group-open:rotate-180">
+					<svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path></svg>
+				</span>
+					</summary>
+					<p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
+						We may offer discounts or promotions from time to time. To stay up-to-date on the latest
+						deals and special offers, you can sign up for the company's newsletter or follow it on social media.
+					</p>
+				</details>
+			</div>
+			<div class="py-5">
+				<details class="group">
+					<summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+						<span> Can you customize a managed IT services plan for my specific business needs?</span>
+						<span class="transition group-open:rotate-180">
+					<svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path></svg>
+				</span>
+					</summary>
+					<p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
+						This platform is a highly reliable and feature-rich service that offers a wide range
+						of tools and functionality. It is competitively priced and offers a variety of billing options to
+						suit different needs and budgets.
+					</p>
+				</details>
+			</div>
 		</div>
 	</div>
-</div>
-</template>
+	</template>
 
-<script>
-    export default {
-        	extend: {
-    keyframes: {
-         fadeIn: {
-       "0%": { opacity: 0 },
-       "100%": { opacity: 100 },
-     },
-   },
-   animation: {
-         fadeIn: "fadeIn 0.2s ease-in-out forwards",
-       },
-     },
-        
-    }
-</script>
+	<script>
+		export default {
+				extend: {
+		keyframes: {
+			fadeIn: {
+		"0%": { opacity: 0 },
+		"100%": { opacity: 100 },
+		},
+	},
+	animation: {
+			fadeIn: "fadeIn 0.2s ease-in-out forwards",
+		},
+		},
+			
+		}
+	</script>
 
-<style lang="scss" scoped>
+	<style lang="scss" scoped>
 
-</style>
+	</style>
