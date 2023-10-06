71204ecf0a9f66feb0b1a0a3b6baa9b9ef946d0b
Foddie2 <Bensonagala@live.com>
2023-10-06 14:19:53 +0300

    Contact cleanup

--- components/Contact.vue
+++ components/Contact.vue
@@ -51,7 +51,8 @@
                 <div class="form-group mb-4">
                   <textarea  id="message" v-model="message" 
                     class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-green-400 focus:outline-none"
-                   rows="3" placeholder="Message"></textarea>
+                   rows="3" placeholder="Message">
+                  </textarea>
                 </div>
 
                 <button
