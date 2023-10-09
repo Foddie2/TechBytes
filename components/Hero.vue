a02e6f3e0ecadd65232f86ff2b99065dd3a1e898
unknown <bensonagala@live.com>
2023-10-09 17:26:11 +0300

    Hero reworks

--- components/Hero.vue
+++ components/Hero.vue
@@ -30,7 +30,7 @@
           <NuxtLink to="/our-story">
           <span class="absolute left-0 block w-full h-0 transition-all bg-green-400 opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease group-hover:bg-gradient-to-r from-[#78f2b3] to-[#16f0de]"></span>
           <span class="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
-          <svg class="w-5 h-5 my-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+          <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
           </span>
          
           <span class="relative uppercase">Explore now</span>
