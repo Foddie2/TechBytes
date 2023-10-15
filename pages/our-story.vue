10a08bdcae9b158407418be381182a8e7a109bb0
Foddie2 <Bensonagala@live.com>
2023-10-15 19:45:59 +0300

    cleanup

--- pages/our-story.vue
+++ pages/our-story.vue
@@ -1,22 +1,12 @@
 <template>
     <div>
         <Navbar/>
-        <AboutDescription/>
-        <!-- <AboutHero/> -->
-        <FAQ/>
         <Footer/>
 
     </div>
 </template>
 
 <script>
- import { onMounted } from 'vue'
-  import { initFlowbite } from 'flowbite'
-  
-  // initialize components based on data attribute selectors
-  onMounted(() => {
-      initFlowbite();
-  })
     export default {
         
     }
