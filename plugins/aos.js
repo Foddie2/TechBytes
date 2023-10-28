2e49399ce793d1c30be1b3aec47a1a86664fa314
Foddie2 <Bensonagala@live.com>
2023-10-28 22:48:49 +0300

    animation bug fix

--- plugins/aos.js
+++ plugins/aos.js
@@ -1,12 +1,8 @@
-import Vue from 'vue'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-Vue.use(AOS.init())
-
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default ({ app }) => {
   app.AOS = new AOS.init({ 
-    disable: "phone",
     disable: window.innerWidth < 640,
     // offset: 200,
     duration: 600,
