b64a699bcdc8a662b4e1c054c36522047b804c4d
Foddie2 <Bensonagala@live.com>
2023-10-28 17:29:16 +0300

    animation bug fix

--- plugins/aos.js
+++ plugins/aos.js
@@ -1,18 +1,9 @@
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-const globalAOS = AOS.init({
-  // note: your options here
-   startEvent: 'load'
-})
 
-// trigger when the Nuxt page is ready
-window.onNuxtReady(() => {
-   AOS.refresh()
-})
 
 export default ({ app }) => {
-  app.AOS = globalAOS
   app.AOS = new AOS.init({ 
     disable: "phone",
     disable: window.innerWidth < 640,
