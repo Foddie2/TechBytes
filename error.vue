8e2633b7b595e76317bf2240eac8495c12d2a5ad
unknown <bensonagala@live.com>
2024-03-21 16:14:30 +0300

    cleanup

--- error.vue
+++ error.vue
@@ -2,7 +2,9 @@
     <div>
      <div class="mt-7 max-w-sm mx-auto text-center card">
          <p class="mt-7 text-7xl font-bold">{{ error.statusCode }}</p>
-         <p class="mt-7 text-6xl ">Ooops ⚠️ <span class="text-green-700">Development in progress.</span> </p>
+         <p class="mt-7 text-6xl ">Ooops ⚠️ 
+            <span class="text-green-700">Development in progress.</span> 
+        </p>
          <p class="mt-8">  {{ error.message }}</p>
  
          <button @click="handleClearError"
