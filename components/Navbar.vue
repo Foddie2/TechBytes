38681eb5483849ea967ccbd8e0fecbcbe6c07003
unknown <bensonagala@live.com>
2024-04-18 15:03:34 +0300

    cleanup

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -12,7 +12,7 @@
           <span class="logo text-xl font-bold tracking-wide text-stone-950">
             Tech
           </span>
-          <span class="bg-stone-950 text-white rounded-r-lg logo text-xl font-bold tracking-wide hover:bg-blue-700">
+          <span class="bg-stone-950 text-white rounded-r-lg logo text-xl font-bold tracking-wide  ">
           Bytes
           </span>
           <br/>
--- components/WhyUs.vue
+++ components/WhyUs.vue
@@ -5,7 +5,7 @@
     
                 <div class="lg:text-center">
                     <h2
-                        class="font-heading mb-4 bg-orange-100 text-orange-800 px-4 py-2 rounded-lg md:w-64 md:mx-auto text-xs font-semibold tracking-widest text-black uppercase title-font">
+                        class="font-heading mb-4 bg-orange-100 text-black-800 px-4 py-2 rounded-lg md:w-64 md:mx-auto text-xs font-semibold tracking-widest text-black uppercase title-font">
                         Why choose us?
                     </h2>
                     <p class="font-heading mt-2 text-3xl leading-8 font-semibold tracking-tight text-gray-900 sm:text-4xl">
